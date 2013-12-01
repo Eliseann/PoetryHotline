b0834507a60732a7375ee6903dbbd36c2ff66e84
ELISE ACKERMAN <eliseackerman@ELISEs-iMac.local>
2013-12-01 11:47:50 -0800

    first commit

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
  echo("\n");
  ?>
  <Response>
- <Gather action="handle-user-input.php" numDigits="1">
-	 <Play>Hello.mp3</Play>
+ <Gather action="handle-user-input.php" numDigits="7">
+	 <Say>Please enter the number you wish to call</Say>
  </Gather>
  <!-- If user doesn't input anything, prompt and try again. -->
  <Say>Sorry, I didn't get your response.</Say>
